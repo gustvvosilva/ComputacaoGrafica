92df1f7e57086fe20678ce7ed4aa5644313fb3ed
gustavo.soliveira51 <gustavo.soliveira51@CAS685580F274D>
2024-08-21 19:29:44 -0300

    oioioi

--- ComputacaoGrafica2/ComputacaoGrafica2.cpp
+++ ComputacaoGrafica2/ComputacaoGrafica2.cpp
@@ -1,20 +1,64 @@
-// ComputacaoGrafica2.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
+#include <stdio.h>
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
 #include <iostream>
+
+const GLint WIDTH = 800, HEIGHT = 600;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	if (!glfwInit()) {
+		printf("erro no glfw");
+		return 1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Computacao grafica", NULL, NULL);
+	if (!window) {
+		printf("A janela nao pode ser criada");
+		glfwTerminate();
+		return 1;
+	}
+	
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(window, &bufferWidth, &bufferHeight);
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK) {
+		printf("Glew nao foi iniciado");
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return 1;
+	}
+
+	glViewport(0, 0, bufferWidth, bufferHeight);
+	
+	while (!glfwWindowShouldClose(window)) {
+		//cria um evento
+		glfwPollEvents();
+
+		//Define a cor do fundo
+		glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+
+	}
+
+
+
+
+
 }
-
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
